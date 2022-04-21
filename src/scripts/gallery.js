181e36727a78715880c617d2de2f1612305f846c
szatolokin <staszatolokin28@gmail.com>
2022-04-21 17:20:54 +0300

    Update
    Gallery

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,3 +1,4 @@
+import { Fancybox } from "@fancyapps/ui";
 import { useDebug } from "./useDebug";
 
 const [log, logError] = useDebug(true);
@@ -6,6 +7,11 @@ log("gallery");
 
 window.addEventListener("DOMContentLoaded", () => {
   try {
+    log({
+      Fb: Fancybox,
+    });
+
+    Fancybox.bind("[data-fancybox]", {});
   } catch (error) {
     logError(error);
   }

1c7bdd80f7131dfc26132e5aba849e65df35de43
szatolokin <staszatolokin28@gmail.com>
2022-04-22 14:48:15 +0300

    Update
    Gallery

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,7 +1,7 @@
 import { Fancybox } from "@fancyapps/ui";
 import { useDebug } from "./useDebug";
 
-const [log, logError] = useDebug(true);
+const [log, logError] = useDebug(false);
 
 log("gallery");
 
@@ -11,7 +11,42 @@ window.addEventListener("DOMContentLoaded", () => {
       Fb: Fancybox,
     });
 
-    Fancybox.bind("[data-fancybox]", {});
+    const imgs = document.querySelectorAll("[data-gallery]");
+    const maxIndex = Math.max(
+      ...Array.from(imgs).map((img) => img.dataset.gallery)
+    );
+    const count = maxIndex + 1;
+
+    log({
+      imgs,
+      maxIndex,
+      count,
+    });
+
+    const items = [];
+
+    for (let index = 0; index < count; index++) {
+      const img = document.querySelector(`[data-gallery="${index}"]`);
+
+      items.push({
+        src: img.src,
+        type: "image",
+      });
+    }
+
+    log({
+      items,
+    });
+
+    imgs.forEach((img) => {
+      const index = img.dataset.gallery;
+
+      img.addEventListener("click", () => {
+        Fancybox.show(items, {
+          startIndex: index,
+        });
+      });
+    });
   } catch (error) {
     logError(error);
   }

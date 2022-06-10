09ee975fcf0817c6f3246f72d6fd83d9e46fb2e8
szatolokinweb <szatolokin.web@gmail.com>
2022-06-10 11:00:15 +0300

    Update top

---
+++ src/scripts/newTop.js
@@ -0,0 +1,82 @@
+import Swiper, { Parallax, Autoplay } from "swiper";
+import { easeOutCubic as easing } from "./easing";
+import throttle from "lodash/throttle";
+
+const SPEED = 1500;
+const DELAY = 2000;
+
+const PARALLAX = 0.25;
+
+const initParallaxY = (top) => {
+  const findElements = throttle(() => {
+    console.log("find elements");
+
+    return top.querySelectorAll("[data-top-parallax-y]");
+  }, 1000);
+
+  const updateElements = (elements, y) => {
+    elements.forEach(
+      (element) => (element.style.transform = `translate3d(0, ${y}px, 0)`)
+    );
+  };
+
+  let elements = findElements();
+
+  window.addEventListener("scroll", () => {
+    elements = findElements();
+
+    const y = scrollY;
+    const height = innerHeight;
+
+    const progress = Math.min(y / height, 1);
+    const easeProgress = easing(progress);
+
+    const easeY = height * easeProgress;
+
+    updateElements(elements, easeY * PARALLAX);
+  });
+};
+
+const initSwiper = (top) => {
+  const swiperEl = top.querySelector("[data-top-swiper]");
+
+  const swiper = new Swiper(swiperEl, {
+    modules: [Parallax, Autoplay],
+
+    parallax: true,
+
+    speed: SPEED,
+    loop: true,
+    allowTouchMove: false,
+
+    autoplay: {
+      delay: DELAY,
+      disableOnInteraction: false,
+    },
+  });
+
+  const stopAutoplay = () => {
+    swiper.autoplay.stop();
+  };
+  const startAutoplay = () => {
+    swiper.autoplay.start();
+  };
+
+  stopAutoplay();
+
+  return { stopAutoplay, startAutoplay };
+};
+
+const init = () => {
+  const tops = document.querySelectorAll("[data-top]");
+
+  tops.forEach((top) => {
+    const { startAutoplay } = initSwiper(top);
+
+    setTimeout(startAutoplay, 2500);
+
+    initParallaxY(top);
+  });
+};
+
+export { init };

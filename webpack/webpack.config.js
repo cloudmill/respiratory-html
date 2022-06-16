0b70b898626b8b89b694aad8b50a42fe17cad529
szatolokinweb <szatolokin.web@gmail.com>
2022-06-17 02:43:21 +0300

    Update

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -50,7 +50,7 @@ module.exports = (env) => {
             {
               loader: "babel-loader",
               options: {
-                presets: ["@babel/preset-env"],
+                presets: ["@babel/preset-env", "@babel/preset-react"],
               },
             },
           ],

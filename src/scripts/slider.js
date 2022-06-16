07bfb7145d0e400d56162245623cb8b938d706c7
szatolokinweb <szatolokin.web@gmail.com>
2022-06-17 02:42:02 +0300

    Update

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,1 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 console.log("slider");
+
+addEventListener("DOMContentLoaded", () => {
+  if (matchMedia("(min-width: 1280px)").matches) {
+    console.log("slider desktop");
+
+    const container = document.querySelector(".js-slider-root");
+
+    const root = ReactDOM.createRoot(container);
+
+    root.render(<div>Hello, world!</div>);
+  }
+});

b0732014cd807b9bfe75bab9f7cc931e339eb8bc
szatolokinweb <szatolokin.web@gmail.com>
2022-06-17 03:02:33 +0300

    Update

--- src/scripts/slider/Slider.js
+++ src/scripts/slider/Slider.js
@@ -1,1 +1,46 @@
-export const Slider = () => <div>qweqweqwe</div>;
+import React, { useEffect, useState } from "react";
+
+export const Slider = () => {
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    const template = document.querySelector(".js-slider-template");
+    const slides = template.content.querySelectorAll(".js-slider-slide");
+
+    slides.forEach((slide) => {
+      const src = slide.querySelector(".js-slider-src").src;
+      const href = slide.querySelector(".js-slider-href").href;
+      const day = slide.querySelector(".js-slider-day").textContent;
+      const month = slide.querySelector(".js-slider-month").textContent;
+      const label = slide.querySelector(".js-slider-label").textContent;
+      const text = slide.querySelector(".js-slider-text").textContent;
+
+      setSlides((slides) => [
+        ...slides,
+        {
+          src,
+          href,
+          day,
+          month,
+          label,
+          text,
+        },
+      ]);
+    });
+  }, []);
+
+  return (
+    <ul>
+      {slides.map((slide, index) => (
+        <li key={index}>
+          <img src={slide.src} />
+          <a href={slide.href}>qwe</a>
+          <div>{slide.day}</div>
+          <div>{slide.month}</div>
+          <div>{slide.label}</div>
+          <div>{slide.text}</div>
+        </li>
+      ))}
+    </ul>
+  );
+};

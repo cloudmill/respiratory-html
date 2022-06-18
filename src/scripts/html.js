07dd3a9bc5d8c6764a0fee5850418670e4398ad8
szatolokin <staszatolokin28@gmail.com>
2022-06-18 18:29:30 +0300

    Update

--- src/scripts/html.js
+++
@@ -1,6 +0,0 @@
-const updateHtmlModifiers = () => {
-  document.documentElement.classList.remove("html--preload");
-  document.documentElement.classList.add("html--loaded");
-};
-
-export { updateHtmlModifiers };

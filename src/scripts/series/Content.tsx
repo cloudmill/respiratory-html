47fe1f11d2e6a2f3ff90c6dd627bb01a3aa46863
szatolokinweb <szatolokin.web@gmail.com>
2022-06-22 23:56:48 +0300

    Add series features

--- src/scripts/series/Content.tsx
+++ src/scripts/series/Content.tsx
@@ -2,12 +2,30 @@ import React from "react";
 import { Slide } from "./data";
 import { LiSvg } from "../components/LiSvg";
 import { MaskText } from "../components/MaskText";
+import { Feature } from "./data";
 
 export interface Animation {
   prev: Slide;
   next: Slide;
   dir: "prev" | "next";
 }
+
+const Features: React.FC<{ features: Feature[] }> = ({ features }) => (
+  <ul className="series__features">
+    {features.map(({ title, labels }, index) => (
+      <li key={index} className="series__feature">
+        <h3 className="series__feature-title">{title}</h3>
+        <ul className="series__feature-labels">
+          {labels.map((label, index) => (
+            <li key={index} className="series__feature-label">
+              {label}
+            </li>
+          ))}
+        </ul>
+      </li>
+    ))}
+  </ul>
+);
 
 export const Content: React.FC<{
   cur: Slide;
@@ -17,7 +35,7 @@ export const Content: React.FC<{
     <div className="series__main">
       <div className="series__content">
         <p className="series__content-sub-title">Серия респираторов</p>
-        <MaskText className="series__content-title">123 123</MaskText>
+        <Features features={cur.features} />
       </div>
       <div className="series__watch">watch</div>
     </div>

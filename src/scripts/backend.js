61489601f238757a14fb98c7a978fbe115f858e2
Gercin <rockinpeople.rip@gmail.com>
2022-07-08 16:20:54 +0300

    update backend

---
+++ src/scripts/backend.js
@@ -0,0 +1,32 @@
+$(function () {
+    filterClick();
+});
+
+function filterClick() {
+    $(document).on("click", "[data-type=js-filter-click]", function (e) {
+        e.preventDefault();
+
+        let thisObj = $(this),
+            value = thisObj.attr("data-select-value");;
+
+        console.log("filter click");
+
+        console.log(value);
+
+        $.ajax({
+            method: "GET",
+            url: window.location.href,
+            data: {
+                ajax: 1,
+                value: value,
+            },
+            success: function (r) {
+                $(document).find('[data-type=items-container-full]').empty();
+                $(document).find('[data-type=items-container-full]').append($(r));
+            },
+            error: function (r) {
+                console.debug(r);
+            }
+        });
+    });
+}

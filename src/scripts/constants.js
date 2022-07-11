d10f4a0f17c5881ec11fb88116303efe2a149701
szatolokinweb <szatolokin.web@gmail.com>
2022-07-11 16:02:06 +0300

    Update

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -2,3 +2,5 @@ export const SLIDER_DURATION = 1200;
 
 export const SERIES_DURATION = 1000;
 export const SERIES_PARALLAX = 0.5;
+
+export const GALLERY_DURATION = 600;
--- src/scripts/swiperSlider.ts
+++ src/scripts/swiperSlider.ts
@@ -1,7 +1,49 @@
-import Swiper, { SwiperOptions } from "swiper";
+import Swiper, {
+  SwiperOptions,
+  Parallax,
+  Navigation,
+  Pagination,
+} from "swiper";
+import { GALLERY_DURATION } from "./constants";
 
-const getSwiperOptions = (swiperId: string | undefined): SwiperOptions => {
+const getSwiperOptions = (swiperEl: HTMLElement): SwiperOptions => {
+  const swiperId = swiperEl.dataset.swiperSlider;
+
   switch (swiperId) {
+    case "card":
+      const prevEl = swiperEl.querySelector<HTMLElement>(
+        "[data-swiper-slider-prev]"
+      );
+      const nextEl = swiperEl.querySelector<HTMLElement>(
+        "[data-swiper-slider-next]"
+      );
+
+      const paginationSrc: string[] = JSON.parse(
+        swiperEl.dataset.swiperSliderPagination || "[]"
+      );
+      const paginationList = swiperEl.querySelector<HTMLElement>(
+        "[data-swiper-slider-list]"
+      );
+      const paginationRender = (index, className) => `
+        <div class="gallery__bullet ${className}">
+          <img class="gallery__bullet-img" src="${paginationSrc[index]}" />
+        </div>
+      `;
+
+      return {
+        modules: [Parallax, Navigation, Pagination],
+        parallax: true,
+        navigation: {
+          prevEl,
+          nextEl,
+        },
+        speed: GALLERY_DURATION,
+        pagination: {
+          el: paginationList,
+          clickable: true,
+          renderBullet: paginationRender,
+        },
+      };
     case "0":
       return {
         loop: true,
@@ -18,10 +60,7 @@ const start = () => {
   );
 
   swiperEls.forEach((swiperEl) => {
-    const swiperId = swiperEl.dataset.swiperSlider;
-    console.log(swiperId);
-
-    const swiper = new Swiper(swiperEl, getSwiperOptions(swiperId));
+    const swiper = new Swiper(swiperEl, getSwiperOptions(swiperEl));
   });
 };
 

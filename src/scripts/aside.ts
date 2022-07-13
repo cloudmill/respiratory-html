e8740dd8e25be2ef738660765f5d38ae0b51cb2d
Kirill Matveev <dejar@protonmail.com>
2022-07-13 18:37:12 +0300

    worked on the adaptive

--- src/scripts/aside.ts
+++ src/scripts/aside.ts
@@ -1,35 +1,43 @@
 import debounce from "lodash.debounce";
 import { getPx } from "./utils";
 
+const mediaQuery = window.matchMedia(`(min-width: 1280px)`);
+
 const DEBOUNCE = 100;
 
 const start = () => {
-  const panel = document.querySelector(".card-panel");
-  const asideAll = document.querySelectorAll<HTMLElement>(".aside");
 
-  const getWindowHeight = () => document.documentElement.clientHeight;
-  const getPanelHeight = () => panel?.getBoundingClientRect().height || 0;
+  if (mediaQuery.matches) {
 
-  asideAll.forEach((aside) => {
-    const getAsideTop = () => aside.getBoundingClientRect().top;
+    const panel = document.querySelector(".card-panel");
+    const asideAll = document.querySelectorAll<HTMLElement>(".aside");
 
-    const getAsideHeight = () => {
-      const windowHeight = getWindowHeight();
-      const asideTop = getAsideTop();
-      const panelHeight = getPanelHeight();
+    const getWindowHeight = () => document.documentElement.clientHeight;
+    const getPanelHeight = () => panel?.getBoundingClientRect().height || 0;
 
-      return windowHeight - (panelHeight + asideTop);
-    };
+    asideAll.forEach((aside) => {
+      const getAsideTop = () => aside.getBoundingClientRect().top;
 
-    const updateAside = () => {
-      aside.style.height = getPx(getAsideHeight());
-    };
-    const updateAsideDebounced = debounce(updateAside, DEBOUNCE);
+      const getAsideHeight = () => {
+        const windowHeight = getWindowHeight();
+        const asideTop = getAsideTop();
+        const panelHeight = getPanelHeight();
 
-    updateAsideDebounced();
-    addEventListener("load", () => updateAsideDebounced());
-    addEventListener("resize", () => updateAsideDebounced());
-  });
+        return windowHeight - (panelHeight + asideTop);
+      };
+
+      const updateAside = () => {
+        aside.style.height = getPx(getAsideHeight());
+      };
+      const updateAsideDebounced = debounce(updateAside, DEBOUNCE);
+
+      updateAsideDebounced();
+      addEventListener("load", () => updateAsideDebounced());
+      addEventListener("resize", () => updateAsideDebounced());
+    });
+    
+  }
+
 };
 
 export { start };
